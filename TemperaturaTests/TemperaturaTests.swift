fcece223bac997890f65b1e72c6e39506fe02350
Glenn Raya <rayaglenn@gmail.com>
2020-05-02 16:25:38 +0800

    Initial Commit

---
+++ TemperaturaTests/TemperaturaTests.swift
@@ -0,0 +1,34 @@
+//
+//  TemperaturaTests.swift
+//  TemperaturaTests
+//
+//  Created by Glenn Raya on 5/2/20.
+//  Copyright © 2020 Glenn Raya. All rights reserved.
+//
+
+import XCTest
+@testable import Temperatura
+
+class TemperaturaTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
